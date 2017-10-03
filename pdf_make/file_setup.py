531cbd053970b343dfa58722e62ff5888f0e0850
Jun Nojima <usada.rose@gmail.com>
2017-10-03 23:21:38 +0900

    ファイル存在チェック追加
    ただしファイル名は決め打ちのまま。

--- pdf_make/file_setup.py
+++ pdf_make/file_setup.py
@@ -4,13 +4,18 @@ Created on 2017/09/30
 @author: nozaki
 '''
 
+import os
+
 class fileSetup():
 
     def getFile(self):
         if self == '':
-            return False
+            return 999
         else:
-            return True
+            if os.path.exists(self):
+                return 0
+            else:
+                return 1
 
 if __name__ == '__main__':
     fileSetup()
--- pdf_make/test_check_file.py
+++ pdf_make/test_check_file.py
@@ -10,15 +10,19 @@ class TestCheckFile(unittest.TestCase):
 
 
     def testGetFileSuccess(self):
-        path = 'G:\text\summary.txt'
+        path = 'G:\\text\summary.txt'
         result = file_setup.fileSetup.getFile(path)
-        self.assertTrue(result)
+        self.assertEqual(result, 0)
 
     def testGetFileFailure(self):
+        path = 'hogehoge'
+        result = file_setup.fileSetup.getFile(path)
+        self.assertEqual(result, 1)
+
+    def testGetFileNullPath(self):
         path = ''
         result = file_setup.fileSetup.getFile(path)
-        self.assertFalse(result)
-
+        self.assertEqual(result, 999)
 
 if __name__ == "__main__":
 
